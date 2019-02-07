a0dd82e9e7548fa9b0a4abffb9fa8841f860c5a6
jordi.alonso <jordi.alonso@grupoica.com>
2019-02-07 22:41:29 +0100

    Model user

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = requier('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+
+  name: { type: String, required: [true, 'El nombre es necesario'] },
+  email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+  password: { type: String, required: [true, 'La constraseña es necesario'] },
+  img: { type: String, required: false },
+  role: { type: String, required: true, default: 'USER_ROLE' }
+
+});
+
+module.exports = mongoose.model('User', userSchema);
